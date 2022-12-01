e475e3e0816fbab236d1845b38767b41242a65e4
JoeZiminski <joseph.j.ziminski@gmail.com>
2022-12-01 13:39:15 +0000

    all stuff

--- python_packaging_at_jz90/__init__.py
+++ python_packaging_at_jz90/__init__.py
@@ -7,6 +7,6 @@ from . import *
 # We can put some package info here that can be accessed
 # with package.__func__ syntax
 __name__ = "JoeZiminski"
-__version__ = "0.0.0.1"  # though nowe we have setuptools_scm
+
 
 
